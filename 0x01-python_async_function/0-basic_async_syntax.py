e302e41d347e2e3d163590fb27dfb35ffdb84cf8
Nico <lawani.gk@gmail.com>
2023-08-04 12:47:11 +0100

    add script to meaure runtime

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,5 @@ async def wait_random(max_delay=10):
     that waits for a random delay between 0 and max_delay
     (included and float value) seconds and eventually returns it."""
     delay = random.uniform(0, max_delay)
-    print('c', delay)
     await asyncio.sleep(delay)
-    print('c', delay)
     return delay
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: float) -> List[float]:
-    """run multiple coroutines at same time"""
+    """Create coroutines and await their completion upon which
+    they will be added to the list to be returned"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
