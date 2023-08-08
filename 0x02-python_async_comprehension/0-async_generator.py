7881dbfb8468e2928262b84551d1034d6d0f4f82
Nico <lawani.gk@gmail.com>
2023-08-08 18:07:11 +0100

    change range of random function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,6 +5,6 @@ import random
 from collections.abc import Iterator
 async def async_generator() -> Iterator[int]:
     """asynchronously generate random values"""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(1, 10)
+        yield random.uniform(0, 10)
