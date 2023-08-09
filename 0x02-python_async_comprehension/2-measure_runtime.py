01ae2ba19dca294f457470be97d656a2ffaf8f80
Nico <lawani.gk@gmail.com>
2023-08-09 08:43:58 +0100

    add type annotation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,7 +5,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     start_time = time.time()
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = time.time()
